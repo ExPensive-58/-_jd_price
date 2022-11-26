43d20304f5ef11f87b7c049687f03c61f1be904b
尒貴ふ <96385808+ExPensive-58@users.noreply.github.com>
2022-11-26 15:04:18 +0800

    Update jd_price.js

--- jd_price.js
+++ jd_price.js
@@ -2,7 +2,6 @@
 #!desc=京东历史价格
 #!system=ios
 
-
 [Script]
 # > 京东比价
 http-response ^https?:\/\/api\.m\.jd\.com\/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) requires-body=1,script-path= https://raw.githubusercontent.com/ExPensive-58/jd_price/main/js
